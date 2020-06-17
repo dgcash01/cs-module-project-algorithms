6b72267f62a3fc40828b9c077ec0d9ce82f2282c
Dan Cashion <dangcashion@gmail.com>
2020-06-17 18:27:57 -0500

    Completed cookie eating.

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,30 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
+def eating_cookies(n, cache=None):
     # Your code here
+    # amount = 3
+    # for x in range(0, n + 1):
+    #     print(x)
 
-    pass
+    cache = [0]*(n+1)      
+    
+    total = 0
+    for i in range(0,len(cache)):
+        if i < 2:
+            cache[i]= 1
+           
+        elif i == 2:
+            cache[i] = cache[i-2] + cache[i-1]
+        else: 
+            cache[i] = cache[i-3] + cache[i-2] + cache[i-1] 
+    return cache[i]
+
+    # return amount
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to eat {num_cookies} cookies")
--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -5,11 +5,48 @@ from collections import namedtuple
 
 Item = namedtuple('Item', ['index', 'size', 'value'])
 
-def knapsack_solver(items, capacity):
+def knapsack_solver(value, weight, capacity):
     # Your code here
 
-    pass
+    n = len(value) - 1
 
+    m = [[-1]*(capacity + 1) for _ in range(n + 1)]
+
+    return knapsack_solver(value, weight, m, n, capacity)
+
+def knapsack_solver(value, weight, m, i, w):
+
+    if m[i][w] >= 0:
+        return m[i][w]
+ 
+    if i == 0:
+        q = 0
+    elif weight[i] <= w:
+        q = max(knapsack_solver(value, weight,
+                                m, i - 1 , w - weight[i])
+                + value[i],
+                knapsack_solver(value, weight,
+                                m, i - 1 , w))
+    else:
+        q = knapsack_solver(value, weight,
+                            m, i - 1 , w)
+    m[i][w] = q
+    return q
+ 
+ 
+n = int(input('Enter number of items: '))
+value = input('Enter the values of the {} item(s) in order: '
+              .format(n)).split()
+value = [int(v) for v in value]
+value.insert(0, None) # so that the value of the ith item is at value[i]
+weight = input('Enter the positive weights of the {} item(s) in order: '
+               .format(n)).split()
+weight = [int(w) for w in weight]
+weight.insert(0, None) # so that the weight of the ith item is at weight[i]
+capacity = int(input('Enter maximum weight: '))
+ 
+ans = knapsack(value, weight, capacity)
+print('The maximum value of items that can be carried:', ans)
 
 if __name__ == '__main__':
   if len(sys.argv) > 1:
